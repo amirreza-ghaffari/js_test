18d749e21a01ad0f01dd7616c81f82ec2070e6ca
amirxghd <amirreza.ghaffari.d@gmail.com>
2022-08-09 09:54:26 +0430

    delete extra api

--- xx/views.py
+++ xx/views.py
@@ -5,25 +5,7 @@ import requests
 
 # Create your views here.
 
-context = {}
-
 
 def index(request):
 
-    a = {"class": "GraphLinksModel"}
-
-
-    url_transition = 'http://127.0.0.1:8000/diagram/api/v1/transitions/'
-    url_block = 'http://127.0.0.1:8000/diagram/api/v1/blocks/'
-
-    api_call = requests.get(url_transition, headers={})
-    transition = api_call.json()
-
-    api_call = requests.get(url_block, headers={})
-    block = api_call.json()
-    a["nodeDataArray"] = block
-    # a["linkDataArray"] = transition
-
-    a = json.dumps(a)
-
-    return render(request, 'blockEditor.html', {'a': a})
+    return render(request, 'blockEditor.html')
