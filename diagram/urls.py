0cab23a4fb19b70c66c0a45937c854a28b1a4ad1
amirxghd <amirreza.ghaffari.d@gmail.com>
2022-08-29 09:37:22 +0430

    delete extra views

--- diagram/urls.py
+++ diagram/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, include
 from django.contrib.auth import urls
-from .views import approved_block
 
 
 app_name = "diagram"
 
 urlpatterns = [
     path('api/v1/', include('diagram.api.v1.urls')),
-    path('approve_block/<str:block_label>/', approved_block, name='approve_block'),
 
 ]
 
