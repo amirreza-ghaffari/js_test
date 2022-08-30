3ebee6b625f4f25fb5d55c6e5e1f65704efa1932
amirxghd <amirreza.ghaffari.d@gmail.com>
2022-08-30 15:47:14 +0430

    location model

--- flowchart/admin.py
+++ flowchart/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
-from .models import Flowchart
+from .models import Flowchart, Location
 
 # Register your models here.
 
 
 class FlowchartAdmin(admin.ModelAdmin):
-    list_display = ('name', 'location')
-    list_filter = ('name', 'location')
+    list_display = ('name', 'location', 'primary', 'is_active')
+    list_filter = ('name', 'location', 'primary', 'is_active')
     fieldsets = (
-        (None, {'fields': ('name', 'location')}),
+        (None, {'fields': ('name', 'location', 'primary', 'is_active')}),
+    )
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'incident_number')
+    list_filter = ('name', )
+    fieldsets = (
+        (None, {'fields': ('name', 'incident_number')}),
     )
 
 
 admin.site.register(Flowchart, FlowchartAdmin)
+admin.site.register(Location, LocationAdmin)
--- flowchart/models.py
+++ flowchart/models.py
@@ -1,19 +1,26 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
 
 
-location_choices = (
-    ('Danesh', 'Danesh'),
-    ('Danesh2', 'Danesh2'),
-    ('Gandhi', 'Gandhi'),
-    ('Shad Abad', 'Shad Abad'),
-    ('Badamak', 'Badamak'),
-)
+class Location(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    incident_number = models.PositiveIntegerField(default=0)
+
+    def increase_incident(self):
+        self.incident_number += 1
+        self.save()
+        return self.incident_number
+
+    def __str__(self):
+        return self.name
 
 
 class Flowchart(models.Model):
     name = models.CharField(max_length=256, unique=True)
-    location = models.CharField(max_length=256, choices=location_choices, default=None, blank=True, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True)
+    primary = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
     class Meta:
         unique_together = ('name', 'location')
@@ -22,5 +29,12 @@ class Flowchart(models.Model):
     def get_absolute_url(self):
         return reverse('flowchart:flowchart_view', kwargs={'name': self.name})
 
+    def clean(self):
+        if self.location and self.primary:
+            raise ValidationError('Primary Flowchart can not have a location')
+
     def __str__(self):
+        if self.primary:
+            return self.name + 'primary'
         return self.name
+
