3f8abcaf91c3310666a02ae690157bab158196d1
amirxghd <amirreza.ghaffari.d@gmail.com>
2022-08-30 15:47:41 +0430

    dashboard

--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'index'
 urlpatterns = [
-    path('', views.index_view, name='flowchart_view'),
+    path('', views.index_view, name='dashboard'),
 ]
