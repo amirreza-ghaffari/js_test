376e607535b8674780223ba1a84e283a2d3e4d70
amirxghd <amirreza.ghaffari.d@gmail.com>
2022-08-31 11:01:05 +0430

    new Flowchart bug fix

--- flowchart/api/v1/views.py
+++ flowchart/api/v1/views.py
@@ -38,36 +38,82 @@ def incident_per_location(request):
 
     return Response(data)
 
-
-def new_flowchart(request, old_flowchart_name, new_flowchart_name):
-
-    try:
-        old_flowchart_obj = Flowchart.objects.get(name=old_flowchart_name)
-    except Flowchart.DoesNotExist:
-        return Response({'error': 'old flowchart does not exists'}, status=status.HTTP_404_NOT_FOUND)
+@api_view(['Post'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def new_flowchart(request, primary_name, location_name):
 
     try:
-        new_flowchart_obj = Flowchart.objects.get(name=new_flowchart_name)
+        primary_flowchart = Flowchart.objects.get(name=primary_name, primary=True)
     except Flowchart.DoesNotExist:
-        return Response({'error': 'new flowchart does not exists'}, status=status.HTTP_404_NOT_FOUND)
-    blocks = Block.objects.filter(flowchart=old_flowchart_obj)
-    transitions = Transition.objects.filter(flowchart=old_flowchart_obj)
+        return Response({'message': 'Primary Flowchart Does not Exists', 'error': True},
+                        status=status.HTTP_404_NOT_FOUND)
+    try:
+        location_obj = Location.objects.get(name=location_name)
+    except Location.DoesNotExist:
+        return Response({'message': 'Location Does not Exists', 'error': True},
+                        status=status.HTTP_404_NOT_FOUND)
+    if Flowchart.objects.filter(name=primary_flowchart.name, location=location_obj):
+        return Response({'message': 'This Flowchart Already Exists', 'error': True},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    new_flowchart = Flowchart.objects.create(name=primary_flowchart.name, location=location_obj)
+    blocks = Block.objects.filter(flowchart=primary_flowchart)
+    transitions = Transition.objects.filter(flowchart=primary_flowchart)
+
+    data = {}
 
     for block in blocks:
         block.pk = None
         block.is_approved = False
         block.is_active = False
-        block.flowchart = new_flowchart_obj
+        block.flowchart = new_flowchart
         block.save()
+        data[block.label] = block.id
+    block = Block.objects.get(label='شروع', flowchart_id=new_flowchart.id)
+    block.is_active = True
+    block.save()
 
     for transition in transitions:
         transition.pk = None
         transition.is_active = False
         transition.is_approved = False
-        transition.flowchart = new_flowchart_obj
+        start_block_name = transition.start_block.label
+        end_block_name = transition.end_block.label
+        transition.start_block_id = data[start_block_name]
+        transition.end_block_id = data[end_block_name]
+        transition.flowchart = new_flowchart
         transition.save()
 
-    return Response({'detail': 'new objects created'}, status=status.HTTP_201_CREATED)
+    return Response({'message': 'new objects created', 'new_flowchart_id': new_flowchart.id},
+                    status=status.HTTP_201_CREATED)
 
+
+@api_view(['Post'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def reset_flowchart(request):
+    flowchart_name = request.data.get('flowchart_name')
+    if not flowchart_name:
+        return Response({'message': 'flowchart_name must be included', 'error': True},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    if not Flowchart.objects.filter(name=flowchart_name).exists():
+        return Response({'message': 'flowchart does not exists', 'error': True},
+                        status=status.HTTP_400_BAD_REQUEST)
+    blocks = Block.objects.filter(flowchart__name=flowchart_name)
+    for block in blocks:
+        block.is_active = False
+        block.is_approved = False
+        block.save()
+    b = Block.objects.get(label='شروع', flowchart__name=flowchart_name)
+    b.is_active = True
+    b.save()
+    transitions = Transition.objects.filter(flowchart__name=flowchart_name)
+    for tr in transitions:
+        tr.is_approved = False
+        tr.is_active = False
+        tr.save()
+    return Response({'message': 'Flowchart rested successfully', 'error': False}, status=status.HTTP_200_OK)
 
 
