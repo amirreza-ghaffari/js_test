182d2373f1bb77f651a98e6c4e9ba7a8ea3bd59b
amirxghd <amirreza.ghaffari.d@gmail.com>
2022-09-07 09:31:15 +0430

    bg fix

--- diagram/utils.py
+++ diagram/utils.py
@@ -1,31 +1,20 @@
 from django.conf import settings
-from django.core.mail import send_mail
-from .models import Block, Transition
+from .models import Block
 from django.template.loader import render_to_string
-from django.core.mail import EmailMessage
-from django.utils.html import strip_tags
 from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
 
 
 html_version = 'email/rac/rac.html'
 
 
-def custom_send_email(message, current_action, next_action, to_email_list, subject='BCM Management'):
+def custom_send_email(message, current_action, next_action, to_email_list, random_text, subject='BCM Management'):
 
-    context = {'current_action': current_action, 'next_action': next_action}
+    context = {'current_action': current_action, 'next_action': next_action, 'random_text': random_text}
     message = render_to_string(html_version, context=context)
-    email = EmailMultiAlternatives(subject, message , settings.EMAIL_HOST_USER, to_email_list)
+    email = EmailMultiAlternatives(subject, message, settings.EMAIL_HOST_USER, to_email_list)
     email.content_subtype = 'html'  # this is required because there is no plain text email version
     email.send(fail_silently=False)
-
-
-
-def next_block(transient):
-    return transient.end_block
-
-def next_transitions(block):
-    return Transition.objects.filter(start_block=block)
-
 
 
 def find_next_action(block, group):
