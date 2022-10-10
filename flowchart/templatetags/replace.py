44e39efc1569df6ae23863a98ebe158df81e8e44
amirxghd <amirreza.ghaffari.d@gmail.com>
2022-10-10 09:37:20 +0330

    add new functions

--- flowchart/templatetags/replace.py
+++ flowchart/templatetags/replace.py
@@ -3,7 +3,7 @@ register = template.Library()
 
 
 @register.filter
-def replace(value):
+def replace_active(value):
     """
     Replacing filter
     Use `{{ "aaa"|replace:"a|b" }}`
@@ -13,3 +13,25 @@ def replace(value):
     if value == 'is_approved':
         return value.replace('is_approved', 'تایید شده')
     return value
+
+
+@register.filter
+def replace(value, arg):
+    """
+    Replacing filter
+    Use `{{ "aaa"|replace:"a|b" }}`
+    """
+    if len(arg.split('|')) != 2:
+        return value
+
+    what, to = arg.split('|')
+    return value.replace(what, to)
+
+
+@register.filter
+def upper_case(value):
+    value = value.replace('_', ' ')
+    return value.title()
+
+
+
