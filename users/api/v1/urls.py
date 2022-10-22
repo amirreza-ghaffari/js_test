2639a337a73ff599b84ee570c301d9e8cb70a3f6
amirxghd <amirreza.ghaffari.d@gmail.com>
2022-10-22 12:07:33 +0330

    add send sms url

--- users/api/v1/urls.py
+++ users/api/v1/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('members/', views.MemberViewSet.as_view({'get':"list"}), name='members'),
     path('send-custom-sms/', views.send_sms_api, name='send-custom-sms'),
     path('send-custom-email/', views.send_email_api, name='send-custom-email'),
+    path('send-msg/', views.send_msg, name='send-smg')
 
     ]
