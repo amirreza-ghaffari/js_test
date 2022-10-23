1fede14a4ffeb50cd1779deac248d2c574a5a56e
amirxghd <amirreza.ghaffari.d@gmail.com>
2022-10-23 10:55:53 +0330

    add send msg in one api

--- users/api/v1/views.py
+++ users/api/v1/views.py
@@ -19,45 +19,6 @@ class MemberViewSet(ModelViewSet):
 @api_view(['POST'])
 @authentication_classes([SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
-def send_sms_api(request):
-    members_id = request.data.get('members')
-    members_id = ast.literal_eval(members_id)
-    members_id = [int(x) for x in members_id]
-
-    message = request.data.get('message')
-    members_mobile_lst = list(Member.objects.filter(id__in=members_id).values_list('mobile_number', flat=True))
-    try:
-        x = send_sms(phone_number_lst=members_mobile_lst, message=message)
-        return Response({'message': 'message sent'}, status=status.HTTP_200_OK)
-    except:
-        return Response({'message': 'could not send message'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-@api_view(['POST'])
-@authentication_classes([SessionAuthentication, BasicAuthentication])
-@permission_classes([IsAuthenticated])
-def send_email_api(request):
-    try:
-        members_id = request.data.get('members')
-        members_id = ast.literal_eval(members_id)
-        members_id = [int(x) for x in members_id]
-
-        message = request.data.get('message')
-        members_email_lst = list(Member.objects.filter(id__in=members_id).values_list('email', flat=True))
-        custom_send_email({'message': message}, members_email_lst, template_address='email/mail2.html')
-        return Response({'message': 'message sent'}, status=status.HTTP_200_OK)
-    except:
-        return Response({'message': 'could not send message'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-def send_email(block_obj, user):
-    context = {'current_action': block_obj.label}
-    custom_send_email(context, ['h.pourhaji@digikala.com'])
-
-
-@api_view(['POST'])
-@authentication_classes([SessionAuthentication, BasicAuthentication])
-@permission_classes([IsAuthenticated])
 def send_msg(request):
 
     try:
@@ -69,16 +30,25 @@ def send_msg(request):
         msg_type = ast.literal_eval(msg_type)
         msg_type = [str(x) for x in msg_type]
 
-        block_id = request.data.get('block_id')
+    except:
+        return Response({'message': 'no member or msg_type provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+    block_id = request.data.get('block_id')
+    msg_text = request.data.get('text')
+    if block_id:
         block = Block.objects.get(id=block_id)
+        if msg_text:
+            text = msg_text
+        else:
+            text = block.label
+    else:
+        text = msg_text
 
-        msg_text = request.data.get('text')
-        text = msg_text if msg_text != '' else block.label
-
-        if len(members_id) > 0:
+    print(text)
+    try:
+        if len(members_id) > 0 and text != '' and text:
             queryset = Member.objects.filter(id__in=members_id)
             if 'sms' in msg_type:
-                print('sms text')
                 mobile_lst = list(queryset.values_list('mobile_number', flat=True))
                 status_code = send_sms(mobile_lst, text)
 
@@ -89,4 +59,4 @@ def send_msg(request):
 
         return Response({'message': 'message sent'}, status=status.HTTP_200_OK)
     except:
-        return Response({'message': 'message sent'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'There is a Error'}, status=status.HTTP_400_BAD_REQUEST)
