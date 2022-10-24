77eb0903f4b7047db557f6ccb10571e470babbc3
amirxghd <amirreza.ghaffari.d@gmail.com>
2022-10-24 10:57:57 +0330

    add block_info view

--- diagram/urls.py
+++ diagram/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from django.contrib.auth import urls
+from . import views
 
 
 app_name = "diagram"
 
 urlpatterns = [
     path('api/v1/', include('diagram.api.v1.urls')),
+    path('block_info/<str:pk>/', views.block_info_view, name='block_info'),
 
 ]
 
--- diagram/views.py
+++ diagram/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-
+from diagram.models import Block, Comment
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+
+@login_required(login_url='users:login')
+def block_info_view(request, pk):
+    context = {}
+    block = get_object_or_404(Block, id=pk)
+    comments = Comment.objects.filter(block=block)
+    context['comments'] = comments
+    triggered_date = block.flowchart.triggered_date
+
+    try:
+        history_approve = block.history.filter(history_date__gte=
+                                         triggered_date, is_approved=True).order_by('-history_date').values('history_date',
+                                                                                          'history_user__email').last()
+        context['approved'] = history_approve
+    except:
+        context['approved'] = None
+
+    return render(request, 'diagram/block_info.html', context)
 
 
 
