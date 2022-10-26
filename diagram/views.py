05e287b2f9fbc83d4ac7f96241f6acaeff1d3010
amirxghd <amirreza.ghaffari.d@gmail.com>
2022-10-26 16:56:33 +0330

    add receivers

--- diagram/views.py
+++ diagram/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from diagram.models import Block, Comment
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
-# Create your views here.
 
 
 @login_required(login_url='users:login')
@@ -12,27 +11,14 @@ def block_info_view(request, pk):
     comments = Comment.objects.filter(block=block)
     context['comments'] = comments
     triggered_date = block.flowchart.triggered_date
+    context['receivers'] = block.members.all()
 
     try:
-        history_approve = block.history.filter(history_date__gte=
-                                         triggered_date, is_approved=True).order_by('-history_date').values('history_date',
+        history_approve = block.history.filter(history_date__gte=triggered_date,
+                                               is_approved=True).order_by('-history_date').values('history_date',
                                                                                           'history_user__email').last()
         context['approved'] = history_approve
     except:
         context['approved'] = None
-
     return render(request, 'diagram/block_info.html', context)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
