64795030a67c3e3b17b939cb71e58a53ccccbd04
amirxghd <amirreza.ghaffari.d@gmail.com>
2022-12-28 11:00:30 +0330

    use label instead of block str

--- users/utils.py
+++ users/utils.py
@@ -1,3 +1,4 @@
+import imaplib
 import requests
 import json
 from diagram.models import Block
@@ -5,7 +6,21 @@ from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 from mattermostdriver import Driver
+from email.header import decode_header
+import email
+from bs4 import BeautifulSoup
+from .models import Member, CustomUser, EmailResponse
+from notifications.models import Notification
+from notifications.signals import notify
 
+
+def en2fa(string):
+    if string.lower() == 'people_protest':
+        return 'اعتراضات و اعتصابات مردمی'
+    elif string.lower() == 'staff_strike':
+      return 'شورش'
+
+    return None
 
 def custom_send_email(context, to_email_list, subject='BCM Management', template_address='users/email.html'):
 
@@ -38,7 +53,7 @@ def next_action(block, member):
         if len(next_blocks) == 0:
             return None
         if next_blocks[0] in candidate_blocks:
-            return Block.objects.get(id=next_blocks[0])
+            return Block.objects.get(id=next_blocks[0]).label
         temp_id = next_blocks[0]
 
 
@@ -90,4 +105,73 @@ def mattermost(usernames: list, msg: str):
     })
 
     return None
+
+
+def clean(text):
+    # clean text for creating a folder
+    return "".join(c if c.isalnum() else "_" for c in text)
+
+
+def email_response(n=5):
+    imap = imaplib.IMAP4_SSL('mail.digikala.com')
+    imap.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+
+    status, messages = imap.select("INBOX")
+    # number of top emails to fetch
+    # total number of emails
+    messages = int(messages[0])
+    if messages < n:
+        n = messages
+
+    for i in range(messages, messages - n, -1):
+        # fetch the email message by ID
+        body = None
+        res, msg = imap.fetch(str(i), "(RFC822)")
+        for response in msg:
+            if isinstance(response, tuple):
+                # parse a bytes email into a message object
+                msg = email.message_from_bytes(response[1])
+                # decode the email subject
+                subject, encoding = decode_header(msg["Subject"])[0]
+                if isinstance(subject, bytes):
+                    # if it's a bytes, decode to str
+                    subject = subject.decode(encoding)
+                # decode email sender
+                from_, encoding = decode_header(msg.get("From"))[0]
+                if isinstance(from_, bytes):
+                    from_ = from_.decode(encoding)
+                # print("Subject:", subject)
+                # print("From:", from_)
+                # if the email message is multipart
+                if msg.is_multipart():
+                    # iterate over email parts
+                    for part in msg.walk():
+                        try:
+                            # get the email body
+                            body = part.get_payload(decode=True).decode()
+                        except:
+                            pass
+                else:
+                    body = msg.get_payload(decode=True).decode()
+                if body:
+                    soup = BeautifulSoup(body, "html.parser")
+                    res_text = soup.find("body").text.split("From: Digikala Crisis.software")[0].strip().replace('\n\n', '')
+                    member_email = from_.split('<')[1][:-1]
+                    try:
+                        member = Member.objects.get(email__iexact=member_email.lower())
+                        email_res_obj, created = EmailResponse.objects.get_or_create(member=member, message=res_text)
+                        if created:
+                            notify.send(CustomUser.objects.first(), recipient=CustomUser.objects.all(), verb='you reached level 10', description=res_text)
+
+                    except Member.DoesNotExist:
+                        pass
+
+    # close the connection and logout
+    imap.close()
+    imap.logout()
+
+
+
+
+
 
