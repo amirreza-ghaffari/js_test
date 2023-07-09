4ddb381ac4f68ab263f2c9e845c5c73b740c9c86
amir <amirreza.ghaffari.d@gmail.com>
2023-07-09 15:54:31 +0330

    Subject Change

--- users/utils.py
+++ users/utils.py
@@ -36,7 +36,7 @@ def en2fa(string):
     return ''
 
 
-def custom_send_email(context, to_email_list, flowchart_id=None, subject='BCM Management',
+def custom_send_email(context, to_email_list, flowchart_id=None, subject='Crisis Contingency Plan',
                       template_address='users/email.html'):
     rand_string = str(''.join(random.choices(string.ascii_uppercase + string.digits, k=7)))
     context['rand_string'] = rand_string
