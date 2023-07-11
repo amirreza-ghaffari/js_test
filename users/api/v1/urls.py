e49bb45e48e654a4dd7d5cbd5890c1bc02828c7d
amir <amirreza.ghaffari.d@gmail.com>
2023-07-11 15:10:45 +0330

    add caller url

--- users/api/v1/urls.py
+++ users/api/v1/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('members/', views.MemberViewSet.as_view({'get': "list"}), name='members'),
     path('send-msg/', views.send_block_msg, name='send-smg'),
     path('custom-send-smg/', views.custom_send_smg, name='custom_send_smg'),
+    path('caller/', views.call_members, name='call_members'),
     ]
